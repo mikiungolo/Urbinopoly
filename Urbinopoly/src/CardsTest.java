420edd00d4d98f4be875f8b852e108cc193300e9
MirianaSette <settemiriana@gmail.com>
2022-12-06 20:01:15 +0100

    RELOAD

---
+++ Urbinopoly/src/CardsTest.java
@@ -0,0 +1,26 @@
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class CardsTest {
+
+    ProbabilityDeck p = new ProbabilityDeck();
+
+    @Test
+    public void testGetEvento() {
+
+        // Cards.CardApi corr = null;
+        // Cards.CardApi prec = null;
+        // for(int i = 0; i <= 20; i++){
+        //     corr = p.getEvento();
+        //     assertFalse(null, corr.equals(prec));
+        //     prec = corr;
+        // }
+
+
+    }
+
+}
