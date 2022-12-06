ed6b711b4146fae905f2ec651e2721d7e6af7534
Michelangelo Ungolo <ungolomiki46@gmail.com>
2022-12-06 17:38:08 +0100

    test 1

--- Urbinopoly/src/DiceTest.java
+++ Urbinopoly/src/DiceTest.java
@@ -1,11 +1,13 @@
-package GameTest;
+import static org.junit.Assume.assumeTrue;
 
 import org.junit.Test;
 
 public class DiceTest {
+    Dice d = new Dice();
+
     @Test
     public void testIsDouble() {
-
+        assumeTrue(d.roll() <= 12 && d.roll() >= 2);
     }
 
     @Test
