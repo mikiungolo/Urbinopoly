9420ccd9d84a765bbb8c7fa189b0ecf7e4ecf14e
Michelangelo Ungolo <ungolomiki46@gmail.com>
2022-12-06 19:05:17 +0100

    Revert "test 1"
    This reverts commit ed6b711b4146fae905f2ec651e2721d7e6af7534.

--- Urbinopoly/src/GameTest/DiceTest.java
+++ Urbinopoly/src/GameTest/DiceTest.java
@@ -1,13 +1,11 @@
-import static org.junit.Assume.assumeTrue;
+package GameTest;
 
 import org.junit.Test;
 
 public class DiceTest {
-    Dice d = new Dice();
-
     @Test
     public void testIsDouble() {
-        assumeTrue(d.roll() <= 12 && d.roll() >= 2);
+
     }
 
     @Test
